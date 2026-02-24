3484c2dc7b78662d865cb7a5147ae679a4d782dd
DanielSaidov123 <ddny3622@gmail.com>
2026-02-24 14:59:54 +0200

    add

--- client/src/components/HomeRgister.jsx
+++ client/src/components/HomeRgister.jsx
@@ -1,6 +1,16 @@
-
 export const HomeRgister = () => {
   return (
-    <div> </div>
-  )
-}
+    <div className="divform">
+      <div className="buten">
+        <button>login</button>
+        <button>register</button>
+      </div>
+      <form className="formRegister">
+        <label htmlFor="email">Emile</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Passeord</label>
+        <input type="password" id="password" />
+      </form>
+    </div>
+  );
+};
