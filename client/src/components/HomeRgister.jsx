4587ca294345a757bbefd211096e9d96ca414e97
DanielSaidov123 <ddny3622@gmail.com>
2026-02-24 14:38:01 +0200

    add Navbar and file

---
+++ client/src/components/HomeRgister.jsx
@@ -0,0 +1,6 @@
+
+export const HomeRgister = () => {
+  return (
+    <div> </div>
+  )
+}
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav className='nav'>
+        <h1>🛡️Secure Notes App</h1>
+    </nav>
+  )
+}
